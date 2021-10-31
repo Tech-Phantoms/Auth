d01b1f4f6f45180705d68a561a2cc092cd090574
Souvikns <souvikde.ns@gmail.com>
2021-10-31 22:05:59 +0500

    fix(lambda): removed lambda function

--- src/lambda/hello.js
+++
@@ -1,7 +0,0 @@
-export function handler(event, context, callback) {
-  console.log("query string parameters", event.queryStringParameters);
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: "Hello, World!" }),
-  });
-}
