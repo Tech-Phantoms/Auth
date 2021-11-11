0187200f1a03687063b2b0cea7b29a9ac8cedcea
Ndmtuan <tuanb1812847@student.ctu.edu.vn>
2021-11-11 12:28:51 +0700

    LoginPage

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -8,34 +8,18 @@ const Login = ({ login }) => {
     
     return (
         <div className = "loginbg">
-        
+       
         <Form className = "sigin">
+        <h1>Tech-Phantoms</h1>
         <img className="img"src="banner.png" alt="haha" />
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-      
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+
+        <Button className ="box" variant="primary" type="submit" to href="https://github.com/login">
+          Login with Github
         </Button>
       </Form>
+  
       </div>
     )
-  
-    
-
-
     
 };
 
